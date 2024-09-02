42b4ccef4958df9cfeba3c59797ffe89265c23a3
ARORA-PRACHI <prachi659.be22@chitkara.edu.in>
2024-09-02 11:18:11 +0530

    First commit

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,11 +1,12 @@
 const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
-const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+const { addIncome, getIncomes, deleteIncome,updateIncome } = require('../controllers/income');
 
 const router = require('express').Router();
 
 
 router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
+    .put('/update-income/:id', updateIncome)
     .delete('/delete-income/:id', deleteIncome)
     .post('/add-expense', addExpense)
     .get('/get-expenses', getExpense)
